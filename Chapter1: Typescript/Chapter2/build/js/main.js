fdbd68983bb71ae059b7228a806e86572f19c655
Aryan J <aryanjha813@gmail.com>
2025-12-08 00:34:45 +0530

    First commit

--- Chapter1: Typescript/Chapter2/build/js/main.js
+++ Chapter1: Typescript/Chapter2/build/js/main.js
@@ -1,3 +1,10 @@
 let myName = "Aryan";
+let isLoading;
+let meaningofLife;
+let album;
+isLoading = true;
+album = 2009;
+album = "Linkin Park";
+meaningofLife = 42;
 export {};
 //# sourceMappingURL=main.js.map
--- Chapter1: Typescript/Chapter2/src/main.ts
+++ Chapter1: Typescript/Chapter2/src/main.ts
@@ -1,8 +1,9 @@
  let myName: string = "Aryan"
  let isLoading: boolean;
  let meaningofLife: number;
+ let album: any;
 
  isLoading = true;
-
+ album = 2009;
+ album = "Linkin Park"
  meaningofLife = 42;
- 
